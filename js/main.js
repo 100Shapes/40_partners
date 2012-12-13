808b84ba09695c364f5714641347a598c119b98b
Paolo Memoli <paolo@onehundredshapes.com>
2012-12-13 18:35:13 +0000

    added JS to link buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+$body = $('body');
 
+$('a.jump-link').click(function(evnt) {
+  evnt.preventDefault();
+  $body.animate({
+    scrollTop: $(this.hash).offset().top
+  }, 500);
+  return false;
+});
