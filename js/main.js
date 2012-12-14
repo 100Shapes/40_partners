7466aebe1bdc792eef01289aadbd3e6f77cd73c8
Michele Memoli <michele@onehundredshapes.com>
2012-12-14 18:28:43 +0000

    Added a read more link

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
-$body = $('body');
+var $body, $more, $bio, expandedHeight;
 
-$('a.jump-link').click(function(evnt) {
-  evnt.preventDefault();
-  $body.animate({
-    scrollTop: $(this.hash).offset().top
-  }, 500);
-  return false;
+$body = $('body');
+$more = $('.read-more');
+$bio = $('.bio');
+
+// JUMP links
+$('.jump-link').click(function(evnt) {
+	evnt.preventDefault();
+	$body.animate({
+		scrollTop: $(this.hash).offset().top
+	}, 500);
+	return false;
 });
+
+
+// Read more
+expandedHeight = 607;
+
+$more.click(function(evnt) {
+
+	$bio
+		.css({
+			// Set height to prevent instant jumpdown when max height is removed
+			"height": $bio.height(),
+			"max-height": 9999
+		})
+		.animate({
+			"height": expandedHeight
+		});
+
+	$more.fadeOut();
+
+	return false;
+});
+
